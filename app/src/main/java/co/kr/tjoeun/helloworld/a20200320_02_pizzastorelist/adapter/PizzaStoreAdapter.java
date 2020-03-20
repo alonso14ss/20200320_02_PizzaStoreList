d10860602cd3f972ba979876accbe825f7e8ba09
alonso14ss <reiyan92@gmail.com>
2020-03-20 12:50:40 +0900

    인터넷 권한 획득 => 웹상 로고 출력  - 파파존스 로고는 아직 안나옴  oreo이후의 폰

--- app/src/main/java/co/kr/tjoeun/helloworld/a20200320_02_pizzastorelist/adapter/PizzaStoreAdapter.java
+++ app/src/main/java/co/kr/tjoeun/helloworld/a20200320_02_pizzastorelist/adapter/PizzaStoreAdapter.java
@@ -5,10 +5,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+
+import com.bumptech.glide.Glide;
 
 import java.util.List;
 
@@ -36,9 +39,15 @@ public class PizzaStoreAdapter extends ArrayAdapter<PizzaStore> {
         View row = convertView;
 
         if (row == null) {
-            inf.inflate(R.layout.pizza_store_list_item, null);
+            row = inf.inflate(R.layout.pizza_store_list_item, null);
         }
         PizzaStore data = mList.get(position);
+        ImageView logoImg = row.findViewById(R.id.logoImg);
+        TextView nameTxt = row.findViewById(R.id.NameTxt);
+
+        nameTxt.setText(data.getStoreName());
+
+        Glide.with(mContext).load(data.getLogoUrl()).into(logoImg);
 
         return row;
     }
