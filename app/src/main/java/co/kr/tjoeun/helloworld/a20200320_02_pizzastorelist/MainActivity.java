58f13b3457600668e59166bfb7d95ea0635a8cbe
alonso14ss <reiyan92@gmail.com>
2020-03-20 11:44:21 +0900

    일단 시작

---
+++ app/src/main/java/co/kr/tjoeun/helloworld/a20200320_02_pizzastorelist/MainActivity.java
@@ -0,0 +1,31 @@
+package co.kr.tjoeun.helloworld.a20200320_02_pizzastorelist;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.os.Bundle;
+
+import co.kr.tjoeun.helloworld.a20200320_02_pizzastorelist.databinding.ActivityMainBinding;
+
+public class MainActivity extends BaseActivity {
+
+    ActivityMainBinding binding = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        setupEvents();
+        setValues();
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+}
