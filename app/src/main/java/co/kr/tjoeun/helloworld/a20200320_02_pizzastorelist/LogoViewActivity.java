2e54e5274e4919489b3f2740d1f94a9b57fc92cc
alonso14ss <reiyan92@gmail.com>
2020-03-20 15:57:23 +0900

    로고 확대 화면 생성

---
+++ app/src/main/java/co/kr/tjoeun/helloworld/a20200320_02_pizzastorelist/LogoViewActivity.java
@@ -0,0 +1,31 @@
+package co.kr.tjoeun.helloworld.a20200320_02_pizzastorelist;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.os.Bundle;
+
+import co.kr.tjoeun.helloworld.a20200320_02_pizzastorelist.databinding.ActivityLogoViewBinding;
+
+public class LogoViewActivity extends BaseActivity {
+
+    ActivityLogoViewBinding binding = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_logo_view);
+        setupEvents();
+        setValues();
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+}
